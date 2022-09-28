2c5960db9de4801b5fb1003aa0e01b45adc6e97e
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-28 19:34:01 -0400

    fix run away decimal issue in Pizza.price

--- js/scripts.js
+++ js/scripts.js
@@ -71,12 +71,11 @@ function Pizza(toppings, sauce, cheese, size) {
 }
 
 Pizza.prototype.getPrice = function() {
-  console.log(this);
   const taxRate = 1.085;
   let basePrice = basePriceBySize(this.size);
   let toppingCount = this.toppings.length;
   let taxPrice = (Math.ceil(100*(basePrice * (taxRate - 1))))/100;
-  this.price = basePrice + toppingCount*1.15 + taxPrice;
+  this.price = basePrice + taxPrice + toppingCount*1.15;
 };
 
 
